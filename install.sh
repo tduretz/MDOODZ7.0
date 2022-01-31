06e47207185692cea8c9986c751dfaeb8669c914
Roman Kulakov <roman.kulakov@ecapital.com>
2022-01-31 10:42:18 +0100

    save cmake files

--- install.sh
+++
@@ -1,4 +0,0 @@
-git clone https://github.com/jlblancoc/suitesparse-metis-for-windows lib/SuiteSparse
-cmake -S lib/SuiteSparse -B lib/SuiteSparse/_build
-cd lib/SuiteSparse/_build
-make amd umfpack cholmod ccolamd btf lapack cxsparse cblas
