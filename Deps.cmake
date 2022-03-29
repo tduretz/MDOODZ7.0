1203733d4d0ffc61d808cf2b3aa061e03b5f4a2c
Roman Kulakov <roman.kulakov@ecapital.com>
2022-03-29 07:40:53 +0200

    add if hdf5 not found

--- Deps.cmake
+++ Deps.cmake
@@ -19,9 +19,14 @@ set(DEPENDENCIES
         SuiteSparse::umfpack)
 
 include(FindHDF5)
-find_package(HDF5 COMPONENTS C)
-set(DEPENDENCIES ${DEPENDENCIES} ${HDF5_LIBRARIES})
-include_directories(${HDF5_INCLUDE_DIR})
+if (HDF5_FOUND)
+    find_package(HDF5 COMPONENTS C)
+    set(DEPENDENCIES ${DEPENDENCIES} ${HDF5_LIBRARIES})
+    include_directories(${HDF5_INCLUDE_DIR})
+else()
+    set(HDF5_DIR ${PROJECT_SOURCE_DIR}/lib/hdf5/install/share/cmake)
+    FIND_PACKAGE(SuiteSparse CONFIG)
+endif()
 
 if (OMP)
     set(OpenMP_lgomp_LIBRARY "lgomp")
