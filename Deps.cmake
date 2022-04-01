bd71f0eb8f02ad6758000cfbbc5b38a56c33da92
Roman Kulakov <roman.kulakov@ecapital.com>
2022-04-01 17:49:58 +0200

    include env

--- Deps.cmake
+++ Deps.cmake
@@ -1,9 +1,36 @@
-include(FindSuiteSparse.cmake)
-message(SuiteSparse_LIBRARIES=${SuiteSparse_LIBRARIES})
-message(SuiteSparse_INCLUDE_DIRS=${SuiteSparse_INCLUDE_DIRS})
-
-set(DEPENDENCIES ${SuiteSparse_LIBRARIES})
-include_directories(${SuiteSparse_INCLUDE_DIRS})
+if (EXISTS ${PROJECT_SOURCE_DIR}/deps/suitesparse/install/lib/cmake/suitesparse-5.4.0)
+    set(SuiteSparse_DIR ${PROJECT_SOURCE_DIR}/deps/suitesparse/install/lib/cmake/suitesparse-5.4.0)
+    FIND_PACKAGE(SuiteSparse CONFIG)
+    set(DEPENDENCIES
+            SuiteSparse::suitesparseconfig
+            SuiteSparse::amd
+            SuiteSparse::btf
+            SuiteSparse::camd
+            SuiteSparse::ccolamd
+            SuiteSparse::colamd
+            SuiteSparse::cholmod
+            SuiteSparse::cxsparse
+            SuiteSparse::umfpack)
+elseif(EXISTS ${PROJECT_SOURCE_DIR}/deps/suitesparse/install/lib64/cmake/suitesparse-5.4.0)
+    set(SuiteSparse_DIR ${PROJECT_SOURCE_DIR}/deps/suitesparse/install/lib64/cmake/suitesparse-5.4.0)
+    FIND_PACKAGE(SuiteSparse CONFIG)
+    set(DEPENDENCIES
+            SuiteSparse::suitesparseconfig
+            SuiteSparse::amd
+            SuiteSparse::btf
+            SuiteSparse::camd
+            SuiteSparse::ccolamd
+            SuiteSparse::colamd
+            SuiteSparse::cholmod
+            SuiteSparse::cxsparse
+            SuiteSparse::umfpack)
+else()
+    include(FindSuiteSparse.cmake)
+    message(SuiteSparse_LIBRARIES=${SuiteSparse_LIBRARIES})
+    message(SuiteSparse_INCLUDE_DIRS=${SuiteSparse_INCLUDE_DIRS})
+    set(DEPENDENCIES ${SuiteSparse_LIBRARIES})
+    include_directories(${SuiteSparse_INCLUDE_DIRS})
+endif()
 
 include(FindHDF5)
 if (HDF5_FOUND)
