1cbb5256aa3e14d9e717c1e2b8c89ae3e40442c8
Roman Kulakov <roman.kulakov@ecapital.com>
2022-04-06 18:16:38 +0200

    verbose

--- env.cmake
+++ env.cmake
@@ -2,6 +2,7 @@ set(C_COMPILER gcc)
 set(SuiteSparse_DIR /usr/lib/x86_64-linux-gnu/)
 
 if (JULIA)
+    set(SuiteSparse_VERBOSE ON)
     set(SuiteSparse_DIR ${CMAKE_INSTALL_PREFIX})
 else()
     set(SuiteSparse_DIR /usr/lib/x86_64-linux-gnu/)
