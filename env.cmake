e326e274322b873f4d2845b50feecd609725585d
Roman Kulakov <roman.kulakov@ecapital.com>
2022-05-03 18:09:20 +0300

    remove env.cmake

--- env.cmake
+++
@@ -1,8 +0,0 @@
-set(C_COMPILER gcc)
-set(SuiteSparse_DIR /usr/lib/x86_64-linux-gnu/)
-
-if (JULIA)
-    set(SuiteSparse_DIR ${CMAKE_INSTALL_PREFIX})
-else()
-    set(SuiteSparse_DIR /usr/lib/x86_64-linux-gnu/)
-endif()
