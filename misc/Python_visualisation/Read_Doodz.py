f77b890fef830017a6f9e894af5be270f384921c
Roman Kulakov <roman.kulakov@ecapital.com>
2022-05-09 11:53:42 +0200

    add correct setup file

--- misc/Python_visualisation/Read_Doodz.py
+++ misc/Python_visualisation/Read_Doodz.py
@@ -2,7 +2,7 @@ import h5py as h5py
 import numpy as np
 import matplotlib.pyplot as mpl
 
-file = h5py.File('/Users/romankulakov/CLionProjects/MDOODZ7/cmake-exec/PinchSwellGSE/Output00160.gzip.h5', 'r')
+file = h5py.File('/Users/romankulakov/CLionProjects/MDOODZ7/cmake-exec/PinchSwellGSE/Output00140.gzip.h5', 'r')
 P    = file['Centers/d']
 xc   = file['/Model/xc_coord']
 zc   = file['/Model/zc_coord']
@@ -22,7 +22,7 @@ mpl.clf()
 mpl.pcolor(xc,zc,P)
 mpl.set_cmap('jet')
 mpl.colorbar()
-# mpl.clim(0,40)
+#mpl.clim(0,0.015)
 mpl.tight_layout()
 #mpl.contour(x,y,h.transpose(), [500])
 #mpl.quiver(xv[::10,::10],yv[::10,::10],hx_itp[::10,::10], hy_itp[::10,::10])
