96d2b31ad0efbfc18be8b9fcc67ff11c9507f2fc
tduretz <48455309+tduretz@users.noreply.github.com>
2022-05-12 09:03:56 +0200

    Add new gse (#43)

---
+++ MDLIB/RheologyDensity.h
@@ -0,0 +1,42 @@
+#ifndef MDOODZ_RHEOLOGYDENSITY_H
+#define MDOODZ_RHEOLOGYDENSITY_H
+
+typedef struct {
+  double *eta;
+  double *d1;
+} LocalIterationMutables;
+
+
+// TODO organize it into subsections
+typedef struct {
+  double Eii;
+  double f_ani;
+  double Ag;
+  double gam;
+  double lam;
+  double cg;
+  double pg;
+  double C_pwl;
+  double n_pwl;
+  double C_gbs;
+  double n_gbs;
+  double C_lin;
+  double n_lin;
+  double m_lin;
+  double C_exp;
+  double ST;
+  double n_exp;
+  double eta_cst;
+  double eta_el;
+
+  int    elastic;
+  int    peierls;
+  int    dislocation;
+  int    diffusion;
+  int    constant;
+  int    gbs;
+  int    phase;
+  double d;
+} LocalIterationParams;
+
+#endif
