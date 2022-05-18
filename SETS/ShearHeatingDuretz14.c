4c9a9c5a3ade3f9f6048c1d5bef23ce70473267d
tduretz <48455309+tduretz@users.noreply.github.com>
2022-05-18 22:16:40 +0200

    add-ShearHeatingDuretz14 (#51)
    * add-ShearHeatingDuretz14
    
    * comments addressed + ready for becoming a new test
    
    * ready for becoming a new test
    
    Co-authored-by: tduretz <imac@geo145.geophysik.uni-frankfurt.de>
    Co-authored-by: Duretz Thibault <thibault.duretz@univ-rennes1.fr>

---
+++ SETS/ShearHeatingDuretz14.c
@@ -0,0 +1,129 @@
+#include "mdoodz.h"
+
+int SetPhase(MdoodzInstance *instance, Coordinates coordinates) {
+  const double radius = instance->model.user1 / instance->scaling.L;
+  if (coordinates.x * coordinates.x + coordinates.z * coordinates.z < radius * radius) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+double SetTemperature(MdoodzInstance *instance, Coordinates coordinates) {
+  return (instance->model.user0 + zeroC)/instance->scaling.T;
+}
+
+double SetDensity(MdoodzInstance *instance, Coordinates coordinates, int phase) {
+  const double T_init = (instance->model.user0 + zeroC) / instance->scaling.T;
+  if (instance->model.eqn_state > 0) {
+    return instance->materials.rho[phase] * (1 - instance->materials.alp[phase] * (T_init - instance->materials.T0[phase]));
+  } else {
+    return instance->materials.rho[phase];
+  }
+}
+
+SetBC SetBCVx(MdoodzInstance *instance, POSITION position, Coordinates coordinates) {
+  SetBC bc;
+  if (instance->model.shear_style == 0) {
+    if (position == S || position == N || position == NE || position == NW || position == SE || position == SW) {
+      bc.value = 0.0;
+      bc.type  = 13;
+    } else if (position == W || position == E) {
+      bc.value = -coordinates.x * instance->model.EpsBG;
+      bc.type  = 0;
+    } else {
+      bc.value = 0.0;
+      bc.type  = -1;
+    }
+  } else {
+    const double Lz = (double) (instance->model.zmax - instance->model.zmin);
+    if (position == S || position == SE || position == SW) {
+      bc.value = -instance->model.EpsBG * Lz;
+      bc.type  = 11;
+    } else if (position == N || position == NE || position == NW) {
+      bc.value = instance->model.EpsBG * Lz;
+      bc.type  = 11;
+    } else if (position == E) {
+      bc.value = 0.0;
+      bc.type  = -12;
+    } else if (position == W) {
+      bc.value = 0.0;
+      bc.type  = -2;
+    } else {
+      bc.value = 0.0;
+      bc.type  = -1;
+    }
+  }
+  return bc;
+}
+
+SetBC SetBCVz(MdoodzInstance *instance, POSITION position, Coordinates coordinates) {
+  SetBC bc;
+  if (instance->model.shear_style == 0) {
+    if (position == W || position == E || position == NE || position == NW || position == SE || position == SW) {
+      bc.value = 0.0;
+      bc.type  = 13;
+    } else if (position == S || position == N) {
+      bc.value = coordinates.z * instance->model.EpsBG;
+      bc.type  = 0;
+    } else {
+      bc.value = 0.0;
+      bc.type  = -1;
+    }
+  } else {
+    if (position == E || position == W || position == NE || position == NW || position == SE || position == SW) {
+      bc.value = 0.0;
+      bc.type = -12;
+    } else if (position == S || position == N) {
+      bc.value = 0.0;
+      bc.type = 0;
+    } else {
+      bc.value = 0.0;
+      bc.type = -1;
+    }
+  }
+  return bc;
+}
+
+SetBC SetBCT(MdoodzInstance *instance, POSITION position, double particleTemperature) {
+  SetBC     bc;
+  if (position == W || position == E || position == S || position == N || position == SE || position == SW || position == NE || position == NW) {
+    bc.type  = 0;
+    bc.value = 0.0;
+  } else {
+    bc.type  = -1;
+    bc.value = 0.0;
+  } 
+  return bc;
+}
+
+
+SetBC SetBCTNew(MdoodzInstance *instance, POSITION position, double particleTemperature) {
+  SetBC     bc;
+  if (position == W || position == E || position == S || position == N || position == SE || position == SW || position == NE || position == NW) {
+    bc.type  = 0;
+    bc.value = 0.0;
+  } else {
+    bc.type  = -1;
+    bc.value = 0.0;
+  } 
+  return bc;
+}
+
+int main(int nargs, char *args[]) {
+  MdoodzInstance instance = {
+          .inputFileName = GetSetupFileName(nargs, args),
+          .SetParticles  = &(SetParticles_ff){
+                   .SetPhase              = SetPhase,
+                   .SetDensity            = SetDensity,
+                   .SetTemperature        = SetTemperature,
+          },
+          .SetBCs = &(SetBCs_ff){
+                  .SetBCVx = SetBCVx,
+                  .SetBCVz = SetBCVz,
+                  .SetBCT    = SetBCT,
+                  .SetBCTNew = SetBCTNew,
+          },
+  };
+  RunMDOODZ(&instance);
+}
