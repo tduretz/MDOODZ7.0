0ac357021f5f3a1f001b049a155ab4a8259f39a0
Roman Kulakov <roman.kulakov@ecapital.com>
2022-08-05 22:53:53 +0400

    fix desc

--- SETS/RiftingPaulineAniso.c
+++ SETS/RiftingPaulineAniso.c
@@ -141,7 +141,7 @@ void AddAnisotropy(MdoodzInput *input, MutateInputParams *mutateInputParams) {
   input->materials.aniso_factor[upperMantlePhase] = mutateInputParams->param7;
   input->materials.aniso_angle[upperMantlePhase]  = mutateInputParams->param8;
   
-  snprintf(input->model.description, sizeof(input->model.description), "Nt: %i CrustalPhase: {cstv: %i, pwlv: %i, aniso_factor: %f, aniso_angle: %f}, UpperMantlePhase: {cstv: %i, pwlv: %i, aniso_factor: %f, aniso_angle: %f}}", input->model.Nt, mutateInputParams->param1, mutateInputParams->param2, mutateInputParams->param3, mutateInputParams->param4, mutateInputParams->param5, mutateInputParams->param6, mutateInputParams->param7, mutateInputParams->param8);
+  snprintf(input->model.description, sizeof(input->model.description), "Nt: %i, CrustalPhase: {cstv: %i, pwlv: %i, aniso_factor: %f, aniso_angle: %f}, UpperMantlePhase: {cstv: %i, pwlv: %i, aniso_factor: %f, aniso_angle: %f}}", input->model.Nt, mutateInputParams->param1, mutateInputParams->param2, mutateInputParams->param3, mutateInputParams->param4, mutateInputParams->param5, mutateInputParams->param6, mutateInputParams->param7, mutateInputParams->param8);
 }
 
 int main() {
