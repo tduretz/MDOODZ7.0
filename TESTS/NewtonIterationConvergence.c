22c539764c8accfb7d6c9b59d49e4d744e2660c6
Roman Kulakov <roman.kulakov@ecapital.com>
2022-09-28 13:33:50 +0200

    refactor

--- TESTS/NewtonIterationConvergence.c
+++ TESTS/NewtonIterationConvergence.c
@@ -45,17 +45,17 @@ void MutateInput(MdoodzInput *input, MutateInputParams *mutateInputParams) {
   }
 }
 
-bool isConvergedInOneIteration(char *hdf5FileName) {
-  hid_t File            = H5Fopen(hdf5FileName, H5F_ACC_RDONLY, H5P_DEFAULT);
-  hid_t IterationsGroup = H5Gopen(File, "Iterations", H5P_DEFAULT);
-  hid_t NumberStepsDataset =
-          H5Dopen(IterationsGroup, "NumberSteps", H5P_DEFAULT);
-  int NumberStepsArray[1] = {0};
-  H5Dread(NumberStepsDataset, H5T_NATIVE_INT, H5S_ALL, H5S_ALL, H5P_DEFAULT,
-          NumberStepsArray);
-  int StepsCount = NumberStepsArray[0];
-  printf("Number of iterations: %d\n", StepsCount);
-  return StepsCount == 1;
+int getStepsCount(char *hdf5FileName) {
+  hid_t file            = H5Fopen(hdf5FileName, H5F_ACC_RDONLY, H5P_DEFAULT);
+  hid_t iterationsGroup = H5Gopen(file, "Iterations", H5P_DEFAULT);
+  hid_t numberStepsDataset =
+          H5Dopen(iterationsGroup, "NumberSteps", H5P_DEFAULT);
+  int numberStepsArray[1] = {0};
+  H5Dread(numberStepsDataset, H5T_NATIVE_INT, H5S_ALL, H5S_ALL, H5P_DEFAULT,
+          numberStepsArray);
+  int stepsCount = numberStepsArray[0];
+  printf("Number of iterations: %d\n", stepsCount);
+  return stepsCount;
 }
 
 int main() {
@@ -82,14 +82,16 @@ int main() {
   mutateInputParams->double1 = 1.0;// matrix nwpl
   RunMDOODZ("ShearTemplate.txt", &setup);
   rename("Output00001.gzip.h5", "lin_pureshear_iso.h5");
-  assert(isConvergedInOneIteration("lin_pureshear_iso.h5"));
+  int stepsCount = getStepsCount("lin_pureshear_iso.h5");
+  assert(stepsCount == 1);
 
   printf("lin_simpleshear_iso");
   mutateInputParams->int1 = 1;// shear_style
   mutateInputParams->int4 = 0;// non-linear
   RunMDOODZ("ShearTemplate.txt", &setup);
   rename("Output00001.gzip.h5", "lin_simpleshear_iso.h5");
-  assert(isConvergedInOneIteration("lin_simpleshear_iso.h5"));
+  stepsCount = getStepsCount("lin_simpleshear_iso.h5");
+  assert(stepsCount == 1);
 
   printf("lin_pureshear_aniso");
   mutateInputParams->int1 = 0;// shear_style
@@ -97,14 +99,16 @@ int main() {
   mutateInputParams->int4 = 0;// non-linear
   RunMDOODZ("ShearTemplate.txt", &setup);
   rename("Output00001.gzip.h5", "lin_pureshear_aniso.h5");
-  assert(isConvergedInOneIteration("lin_pureshear_aniso.h5"));
+  stepsCount = getStepsCount("lin_pureshear_aniso.h5");
+  assert(stepsCount == 1);
 
   printf("lin_simpleshear_aniso");
   mutateInputParams->int1 = 1;// shear_style
   mutateInputParams->int4 = 0;// non-linear
   RunMDOODZ("ShearTemplate.txt", &setup);
   rename("Output00001.gzip.h5", "lin_simpleshear_aniso.h5");
-  assert(isConvergedInOneIteration("lin_simpleshear_aniso.h5"));
+  stepsCount = getStepsCount("lin_simpleshear_aniso.h5");
+  assert(stepsCount == 1);
 
   printf("nonlin_pureshear_iso");
   mutateInputParams->int1 = 0;// shear_style
@@ -112,7 +116,8 @@ int main() {
   mutateInputParams->int4 = 1;// non-linear
   RunMDOODZ("ShearTemplate.txt", &setup);
   rename("Output00001.gzip.h5", "nonlin_pureshear_iso.h5");
-  assert(!isConvergedInOneIteration("nonlin_pureshear_iso.h5"));
+  stepsCount = getStepsCount("nonlin_pureshear_iso.h5");
+  assert(stepsCount > 1);
 
   printf("nonlin_simpleshear_iso");
   mutateInputParams->int1 = 1;// shear_style
@@ -120,7 +125,8 @@ int main() {
   mutateInputParams->int4 = 1;// non-linear
   RunMDOODZ("ShearTemplate.txt", &setup);
   rename("Output00001.gzip.h5", "nonlin_simpleshear_iso.h5");
-  assert(!isConvergedInOneIteration("nonlin_simpleshear_iso.h5"));
+  stepsCount = getStepsCount("nonlin_simpleshear_iso.h5");
+  assert(stepsCount > 1);
 
   printf("nonlin_pureshear_aniso");
   mutateInputParams->int1 = 0;// shear_style
@@ -128,12 +134,14 @@ int main() {
   mutateInputParams->int4 = 1;// non-linear
   RunMDOODZ("ShearTemplate.txt", &setup);
   rename("Output00001.gzip.h5", "nonlin_pureshear_aniso.h5");
-  assert(!isConvergedInOneIteration("nonlin_pureshear_aniso.h5"));
+  stepsCount = getStepsCount("nonlin_pureshear_aniso.h5");
+  assert(stepsCount > 1);
 
   printf("nonlin_simpleshear_aniso");
   mutateInputParams->int1 = 1;// shear_style
   mutateInputParams->int4 = 1;// non-linear
   RunMDOODZ("ShearTemplate.txt", &setup);
   rename("Output00001.gzip.h5", "nonlin_simpleshear_aniso.h5");
-  assert(!isConvergedInOneIteration("nonlin_simpleshear_aniso.h5"));
+  stepsCount = getStepsCount("nonlin_simpleshear_aniso.h5");
+  assert(stepsCount > 1);
 }
