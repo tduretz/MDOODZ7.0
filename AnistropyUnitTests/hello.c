da2e18aff180c1d6aa4aa0a61ea51f350116bcbd
tduretz <imac@geo145.geophysik.uni-frankfurt.de>
2022-10-12 16:12:10 +0200

    clean

--- AnistropyUnitTests/hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <omp.h>
-
-int main(int argc, char** argv){
-    printf("Hello from process: %d\n", omp_get_num_threads());
-    printf("Hello from process: %d\n", omp_get_thread_num());
-
-    return 0;
-}
