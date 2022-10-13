b084bf1dddf74389b4209e3ac1d3262fd6a70fa5
Roman <kulakovri>
2022-10-13 14:05:25 +0200

    fix valfrind errors

--- MDLIB/Main_DOODZ.c
+++ MDLIB/Main_DOODZ.c
@@ -1235,6 +1235,9 @@ void RunMDOODZ(char *inputFileName, MdoodzSetup *setup) {
 
     // Free char*'s
     free(input.model.import_file);
+    free(input.model.import_files_dir);
+    // free(input.model.description);
+    free(input.model.writerSubfolder);
 
     NmodelFree(Nmodel);
 
