86aea303d771779dc8bf96960922461c652a57ab
kulakovri <kulakovri@gmail.com>
2022-10-20 15:49:20 +0200

    add velocity field plots

--- misc/Python_visualisation/Read_Doodz_ShearTemplate_00.py
+++ misc/Python_visualisation/Read_Doodz_ShearTemplate_00.py
@@ -2,7 +2,7 @@ import h5py as h5py
 import numpy as np
 import matplotlib.pyplot as mpl
 
-file = h5py.File('/home/roman/CLionProjects/MDOODZ7.0/cmake-exec/RiftingChenin/result4/Output00200.gzip.h5', 'r')
+file = h5py.File('/home/roman/CLionProjects/MDOODZ7.0/cmake-exec/RiftingChenin/result3/Output00200.gzip.h5', 'r')
 P    = file['Centers/T']
 Vx   = file['VxNodes/Vx']
 Vz   = file['VzNodes/Vz']
