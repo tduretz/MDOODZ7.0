0aa950a7955275537763ff44b53af12562382231
Roman Kulakov <roman.kulakov@ecapital.com>
2023-01-19 11:59:20 +0100

    center layers

--- SETS/Multilayers.c
+++ SETS/Multilayers.c
@@ -20,7 +20,7 @@ void        InitialisePerturbation(MdoodzInput *input) {
     pert_up[il] = calloc(ncx, sizeof(double));
   }
 
-  double A = 0.5 / input->scaling.L;
+  double A = 1 / input->scaling.L;
 
   for (int il = 0; il < nlayers; il++) {
     for (int ic = 30; ic < ncx - 30; ic++) {
@@ -44,29 +44,26 @@ void        InitialisePerturbation(MdoodzInput *input) {
 }
 
 int SetPhase(MdoodzInput *input, Coordinates coordinates) {
-  int    nlayers = (int) input->model.user2;
-  double H       = input->model.user0 / input->scaling.L;
-  int    ncx     = input->model.Nx - 1;
-
   if (!isPerturbationInitialised) {
     InitialisePerturbation(input);
   }
 
-  double spacing = -input->model.user1 / input->scaling.L;
+  const double H            = input->model.user0 / input->scaling.L;
+  const double spacing      = input->model.user1 / input->scaling.L;
+  const int    nlayers      = (int) input->model.user2;
+  const int    ncx          = input->model.Nx - 1;
+  const double layerBoxSize = (nlayers * (H + spacing)) - spacing;
+  const double totalBoxSize = -input->model.zmin + input->model.zmax;
+
+  const double offset = (totalBoxSize - layerBoxSize) / 2;
+
   for (int il = 0; il < nlayers; il++) {
-
-    double layer_bottom = input->model.zmin + 6 / input->scaling.L + il * (H + spacing);
-
-    // Get the column:
+    const double layer_bottom = input->model.zmin + offset + il * (H + spacing);
     int    ic           = ceil(((coordinates.x - (input->model.xmin + input->model.dx / 2)) / input->model.dx) + 0.5) - 1;
     if (ic < 0) ic = 0;
     if (ic > ncx - 1) ic = ncx - 1;
-    // Corresponding layer contour perturbation
-
-
-    double pert_lo_c = pert_lo[il][ic];
-    double pert_up_c = pert_up[il][ic];
-
+    const double pert_lo_c = pert_lo[il][ic];
+    const double pert_up_c = pert_up[il][ic];
     if (coordinates.z > layer_bottom + pert_lo_c && coordinates.z < layer_bottom + H + pert_up_c) {
       return il + 1;
     }
@@ -74,20 +71,11 @@ int SetPhase(MdoodzInput *input, Coordinates coordinates) {
   return 0;
 }
 
-double SetDensity(MdoodzInput *input, Coordinates coordinates, int phase) {
-  const double T_init = (input->model.user0 + zeroC) / input->scaling.T;
-  if (input->model.eqn_state > 0) {
-    return input->materials.rho[phase] * (1 - input->materials.alp[phase] * (T_init - input->materials.T0[phase]));
-  } else {
-    return input->materials.rho[phase];
-  }
-}
 
 int main() {
   MdoodzSetup setup = {
           .SetParticles = &(SetParticles_ff){
                   .SetPhase   = SetPhase,
-                  .SetDensity = SetDensity,
           },
           .SetBCs = &(SetBCs_ff){
                   .SetBCVx = SetPureOrSimpleShearBCVx,
