fd64cec70232f502f1c19d0cb5cb890261e4e881
tduretz <imac@geo145.geophysik.uni-frankfurt.de>
2023-01-26 19:16:35 +0100

    basci setup for necking review

---
+++ SETS/NeckingReview.c
@@ -0,0 +1,138 @@
+#include "math.h"
+#include "mdoodz.h"
+#include "stdbool.h"
+#include "stdlib.h"
+
+
+double SetSurfaceZCoord(MdoodzInput *instance, double x_coord) {
+  const double TopoLevel   = -0.0e3 / instance->scaling.L;
+  const double basin_width = 30.0e3 / instance->scaling.L;
+  const double h_pert      = instance->model.user3 / instance->scaling.L;
+  return TopoLevel + 0*h_pert * exp( - (x_coord*x_coord) / (2.0*basin_width*basin_width)   );
+}
+
+int SetPhase(MdoodzInput *instance, Coordinates coordinates) {
+  const double basin_width = 30.0e3 / instance->scaling.L;
+  const double h_pert      = instance->model.user3 / instance->scaling.L;
+  const double lithosphereThickness  = instance->model.user1 / instance->scaling.L;
+  const double crustThickness        = instance->model.user2 / instance->scaling.L;
+  const double perturbationAmplitude = instance->model.user3 / instance->scaling.L;
+  const double mohoLevel             = -crustThickness + h_pert * exp( - (coordinates.x*coordinates.x) / (2.0*basin_width*basin_width)   );
+  const bool   isBelowLithosphere    = coordinates.z < -lithosphereThickness;
+  const bool   isAboveMoho           = coordinates.z > mohoLevel;
+  const bool   isLowerCrust          = coordinates.z < (mohoLevel + 10.0e3/instance->scaling.L);
+  if (isAboveMoho) {
+    if (isLowerCrust) {
+    return 7;
+    }
+    else {
+    return 1;
+    }
+  } else if (isBelowLithosphere) {
+    return 3;
+  } else {
+    return 2;
+  }
+}
+
+double SetTemperature(MdoodzInput *instance, Coordinates coordinates) {
+  const double lithosphereThickness = instance->model.user1 / instance->scaling.L;
+  const double surfaceTemperature   = 273.15 / instance->scaling.T;
+  const double mantleTemperature    = (1330.0 + 273.15) / instance->scaling.T;
+  const double particleTemperature  = ((mantleTemperature - surfaceTemperature) / lithosphereThickness) * (-coordinates.z) + surfaceTemperature;
+  if (particleTemperature > mantleTemperature) {
+    return mantleTemperature;
+  } else {
+    return particleTemperature;
+  }
+}
+
+double SetGrainSize(MdoodzInput *instance, Coordinates coordinates, int phase) {
+  const int asthenospherePhase = 3;
+  return instance->materials.gs_ref[asthenospherePhase];
+}
+
+double SetHorizontalVelocity(MdoodzInput *instance, Coordinates coordinates) {
+  return -coordinates.x * instance->model.EpsBG;
+}
+
+double SetVerticalVelocity(MdoodzInput *instance, Coordinates coordinates) {
+  return coordinates.z * instance->model.EpsBG;
+}
+
+char SetBCPType(MdoodzInput *instance, POSITION position) {
+  if (position == NE || position == NW) {
+    return 0;
+  } else {
+    return -1;
+  }
+}
+
+SetBC SetBCT(MdoodzInput *instance, POSITION position, double particleTemperature) {
+  SetBC     bc;
+  double surfaceTemperature = zeroC / instance->scaling.T;
+  if (position == FREE_SURFACE) {
+    bc.value = surfaceTemperature;
+    bc.type  = 1;
+  } else {
+    bc.value = 0.0;
+    bc.type  = 0;
+  }
+  return bc;
+}
+
+
+SetBC SetBCTNew(MdoodzInput *instance, POSITION position, double particleTemperature) {
+  SetBC     bc;
+  double surfaceTemperature = zeroC / instance->scaling.T;
+  double mantleTemperature  = (1330. + zeroC) / instance->scaling.T;
+  if (position == S || position == SE || position == SW) {
+    bc.value = particleTemperature;
+    bc.type  = 1;
+  } else if (position == N || position == NE || position == NW) {
+    bc.value = surfaceTemperature;
+    bc.type  = 1;
+  } else if (position == W || position == E) {
+    bc.value = mantleTemperature;
+    bc.type  = 0;
+  } else {
+    bc.value = 0;
+    bc.type  = 0;
+  }
+  return bc;
+}
+
+void AddCrazyConductivity(MdoodzInput *input) {
+  int               *asthenospherePhases = (int *) malloc(sizeof(int));
+  CrazyConductivity *crazyConductivity   = (CrazyConductivity *) malloc(sizeof(CrazyConductivity));
+  asthenospherePhases[0]                 = 3;
+  crazyConductivity->phases              = asthenospherePhases;
+  crazyConductivity->nPhases             = 1;
+  crazyConductivity->multiplier          = 1000;
+  input->crazyConductivity               = crazyConductivity;
+}
+
+int main() {
+  MdoodzSetup setup = {
+          .BuildInitialTopography = &(BuildInitialTopography_ff){
+                  .SetSurfaceZCoord = SetSurfaceZCoord,
+          },
+          .SetParticles = &(SetParticles_ff){
+                  .SetPhase              = SetPhase,
+                  .SetTemperature        = SetTemperature,
+                  .SetGrainSize          = SetGrainSize,
+                  .SetHorizontalVelocity = SetHorizontalVelocity,
+                  .SetVerticalVelocity   = SetVerticalVelocity,
+          },
+          .SetBCs = &(SetBCs_ff){
+                  .SetBCVx    = SetPureShearBCVx,
+                  .SetBCVz    = SetPureShearBCVz,
+                  .SetBCPType = SetBCPType,
+                  .SetBCT     = SetBCT,
+                  .SetBCTNew  = SetBCTNew,
+          },
+          .MutateInput = AddCrazyConductivity,
+
+  };
+  RunMDOODZ("NeckingReview.txt", &setup);
+}
