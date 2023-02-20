ad20dec7ba6d67ee9ea4cdb6889e504d95f6f3cb
William Halter <70947159+halterw@users.noreply.github.com>
2023-02-20 15:44:28 +0100

    Infrastructure: add custom TXT input file (#96)

--- SETS/AddSet.cmake
+++ SETS/AddSet.cmake
@@ -4,6 +4,12 @@ function(add_set SETNAME)
     set_target_properties(${SETNAME}
             PROPERTIES
             RUNTIME_OUTPUT_DIRECTORY ${PROJECT_SOURCE_DIR}/cmake-exec/${SETNAME})
+    if (TXT AND SET)
+        if (NOT EXISTS ${PROJECT_SOURCE_DIR}/cmake-exec/${SETNAME}/${SETNAME}.txt)
+            configure_file("${PROJECT_SOURCE_DIR}/SETS/${TXT}" "${PROJECT_SOURCE_DIR}/cmake-exec/${SETNAME}/${TXT}" COPYONLY)
+        endif()
+    endif()
+
     if (NOT EXISTS ${PROJECT_SOURCE_DIR}/cmake-exec/${SETNAME}/${SETNAME}.txt)
         configure_file("${PROJECT_SOURCE_DIR}/SETS/${SETNAME}.txt" "${PROJECT_SOURCE_DIR}/cmake-exec/${SETNAME}/${SETNAME}.txt" COPYONLY)
     endif()
