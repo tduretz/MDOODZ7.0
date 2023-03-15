bc3221b6f51bbe163ca9e9b9c27f02ace6328f5f
tduretz <duretz@uni-frankfurt.de>
2023-03-15 18:03:31 +0100

    add forgotten files

--- misc/Python_visualisation/Read_Doodz_Temperature.py
+++ misc/Python_visualisation/Read_Doodz_Temperature.py
@@ -1,26 +1,38 @@
 import h5py as h5py
 import numpy as np
 import matplotlib.pyplot as mpl
+from os.path import expanduser
+home = expanduser("~")
+ 
+# Path relative to your home directory
+path ="/REPO/MDOODZ7.0/MDLIB/"
 
-file  = h5py.File('/Users/tduretz/REPO/MDOODZ7.0/MDLIB/Output00000.gzip.h5', 'r')
-T     = file['/Centers/T']
-data  = file['/Model/Params']
-xc    = file['/Model/xc_coord']
-zc    = file['/Model/zc_coord']
-Nx    = data[3].astype(int)
-Nz    = data[4].astype(int)
-Ncx   = Nx-1
-Ncz   = Nz-1
+# File numbers
+file_start = 0
+file_step  = 10
+file_end   = 100
 
-T   = np.reshape( T, (Ncz, Ncx)) - 273.15
+for step in range(file_start, file_end+1, file_step):
+    filename    = home + path + 'Output%05d' % step + '.gzip.h5'
+    print(filename)
+    file   = h5py.File(filename, 'r')
+    T     = file['/Centers/T']
+    data  = file['/Model/Params']
+    xc    = file['/Model/xc_coord']
+    zc    = file['/Model/zc_coord']
+    Nx    = data[3].astype(int)
+    Nz    = data[4].astype(int)
+    Ncx   = Nx-1
+    Ncz   = Nz-1
 
-print(np.shape(T))
-print(np.shape(xc))
-print(np.shape(zc))
-mpl.clf()
-mpl.pcolor(xc, zc, T)
-mpl.set_cmap('jet')
-mpl.colorbar()
-mpl.tight_layout()
-mpl.show()
-mpl.title('T')
+    T   = np.reshape( T, (Ncz, Ncx)) - 0*273.15
+
+    mpl.clf()
+    mpl.pcolor(xc, zc, T)
+    mpl.set_cmap('jet')
+    mpl.colorbar()
+    mpl.tight_layout()
+    mpl.show()
+    mpl.title('T')
+
+    file.close()  
