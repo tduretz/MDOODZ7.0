052efeb68c50a0799101647723e26e0a47b886e3
tduretz <duretz@uni-frankfurt.de>
2023-03-28 17:39:43 +0200

    update pwl ani

--- misc/Python_CodeGen/Rheology/ViscousAnisotropyPowerLawSimple.jl
+++ misc/Python_CodeGen/Rheology/ViscousAnisotropyPowerLawSimple.jl
@@ -9,17 +9,17 @@ function main_simple_ani_vis()
     ε̇xxd       = pure_shear*5.0
     ε̇yyd       = -ε̇xxd
     ε̇xyd       = (1.0-pure_shear)*5.0 + 5.0/3
-    ε̇bg        = sqrt(ε̇xxd^2+ε̇xyd^2)
+    ε̇bg        = sqrt(ε̇xxd^2 + ε̇xyd^2)
     # Power law
-    npwl   = 1
-    τbg    = 2.0
-    Bpwl   = 2^npwl*ε̇bg/τbg^(npwl)
-    τ_chk  = 2*Bpwl^(-1.0/npwl)*ε̇bg^(1.0/npwl)
-    ηpwl   =   Bpwl^(-1.0/npwl)*ε̇bg^(1.0/npwl-1)
-    τ_chk  = 2*ηpwl*ε̇bg
+    npwl      = 10
+    τbg       = 2.0
+    Bpwl      = 2^npwl*ε̇bg/τbg^(npwl)
+    τ_chk     = 2*Bpwl^(-1.0/npwl)*ε̇bg^(1.0/npwl)
+    ηpwl      =   Bpwl^(-1.0/npwl)*ε̇bg^(1.0/npwl-1)
+    τ_chk     = 2*ηpwl*ε̇bg
+    Cpwl      = (2*Bpwl^(-1/npwl))^(-npwl)
     if abs(τbg - τ_chk)/τbg > 1e-6 error("Power-law breaks down") end
-    Cpwl   = (2*Bpwl^(-1/npwl))^(-npwl)
-    
+    # --------------------- TEST 1: Isotropic power law --------------------- #
     # TEST 1: Isotropic power law
     τxx = 2*ηpwl*ε̇xxd
     τxy = 2*ηpwl*ε̇xyd
@@ -27,17 +27,16 @@ function main_simple_ani_vis()
     # Check strain rates
     ε̇xxd_chk = Cpwl*Y2^((npwl-1)/2)*τxx
     ε̇xyd_chk = Cpwl*Y2^((npwl-1)/2)*τxy
-    @show (ε̇xxd, ε̇xxd_chk)
-    @show (ε̇xyd, ε̇xyd_chk)
-
-    # TEST 2: Anisotropic power law
+    println("ε̇xx check: ", ε̇xxd ≈ ε̇xxd_chk) 
+    println("ε̇xy check: ", ε̇xyd ≈ ε̇xyd_chk)
+    # -------------------- TEST 2: Anisotropic power law -------------------- #
     # Arrays
     θ          = LinRange( 0.0, π, 51 ) .-0* π/2
-    τii_cart   = zero(θ)
+    τii_cart1  = zero(θ) 
+    τii_cart2  = zero(θ)
     ε̇ii_rot    = zero(θ)
     τii_rot1   = zero(θ)
     τii_rot2   = zero(θ)
-    τii_rot3   = zero(θ)
     # Loop over all orientations
     for i in eachindex(θ)
         @show θ[i] 
@@ -51,6 +50,7 @@ function main_simple_ani_vis()
         ηpwl        =   Bpwl^(-1.0/npwl)*sqrt(I2)^(1.0/npwl-1)
         D           = 2*ηpwl*[1 0 0; 0 1 0; 0 0 1.0/ani_fac_v;]
         τ           = D*ε̇
+        # ---> Independent on orientation (objective) !!!!!!
         Y2          = 0.5*(τ[1]^2 + τ[2]^2) + τ[3]^2*ani_fac_v^2
         τii_rot1[i] = sqrt(Y2)
         ε̇ii_rot[i]  = sqrt(I2)
@@ -61,32 +61,33 @@ function main_simple_ani_vis()
         ε̇xyd_chk0   = Cpwl*Y2^((npwl-1)/2)*(τxy*ani_fac_v)
         ε̇xxd_chk1   = τxx/2/ηpwl
         ε̇xyd_chk1   = τxy/2/ηpwl*ani_fac_v
-        @show (ε̇_rot[1,1], ε̇xxd_chk0, ε̇xxd_chk1)
-        @show (ε̇_rot[1,2], ε̇xyd_chk0, ε̇xyd_chk1)
+        println( "ε̇xx = ", ε̇_rot[1,1], " ε̇xx check: ", ε̇xxd_chk0≈ε̇_rot[1,1], " ", ε̇xxd_chk1≈ε̇_rot[1,1])
+        println( "ε̇xy = ", ε̇_rot[1,2], " ε̇xy check: ", ε̇xyd_chk0≈ε̇_rot[1,2], " ", ε̇xyd_chk1≈ε̇_rot[1,2])
         # Check strain rate invariant
         ε̇ii_pwl     = Cpwl * sqrt(Y2) ^(npwl)
         ε̇ii_pwl1    = sqrt(ε̇xxd_chk1^2 + ε̇xyd_chk1^2)
-        @show(ε̇ii_pwl, ε̇ii_pwl1)
+        println( "ε̇ii = ", sqrt(I2), " ε̇ii check: ", ε̇ii_pwl≈sqrt(I2), " ", ε̇ii_pwl1≈sqrt(I2))
         # Effective viscosity: compute stress invariant from strain rate invariant
+        # ---> Predicts rotated stress invariant !!!!!!
         I2          = 0.5*(ε̇[1]^2 + ε̇[2]^2) + ε̇[3]^2
         τii_rot2[i] = 2*ηpwl*sqrt(I2)
-        # if one does the same by scaling the strain rate invariant by ani_fac_v...
+        # If one does the same by scaling the strain rate invariant by ani_fac_v^2...
+        # ---> Predicts Cartesian stress invariant !!!!!!
         I2          = 0.5*(ε̇[1]^2 + ε̇[2]^2) + ε̇[3]^2/ani_fac_v^2
-        τii_rot3[i] = 2*ηpwl*sqrt(I2)
-        # Rotate stress back
+        τii_cart1[i] = 2*ηpwl*sqrt(I2)
+        # Rotate stress back 
         τ_rot       = [τ[1] τ[3]; τ[3] τ[2]]
         τ           = Q'*τ_rot*Q
+        # ---> Dependent on orientation (non-objective) !!!!!!
         J2          = 0.5*(τ[1,1]^2 + τ[2,2]^2) + τ[1,2]^2
-        τii_cart[i] = sqrt(J2)  
+        τii_cart2[i] = sqrt(J2)  
     end
     p1 = plot(title="Stress invariant", xlabel="θ", ylabel="τᵢᵢ")
-    p1 = plot!(θ*180/π, τii_cart, label="Cartesian")
-    p1 = plot!(θ*180/π, τii_rot1, label="principal")
-    p1 = plot!(θ*180/π, τii_rot2, label="τii_rot2", linewidth=0, marker =:cross)
-    p1 = plot!(θ*180/π, τii_rot3, label="τii_rot3", linewidth=0, marker =:cross)
-    p1 = plot!(θ*180/π, ε̇ii_rot, label="ε̇ii_rot", linewidth=0, marker =:circle)
+    p1 = plot!(θ*180/π, τii_cart1, label="τii_cart1")
+    p1 = plot!(θ*180/π, τii_cart2, label="τii_cart2", linewidth=0, marker =:cross)
+    p1 = plot!(θ*180/π,  τii_rot1, label="τii_rot1")
+    p1 = plot!(θ*180/π,  τii_rot2, label="τii_rot2", linewidth=0, marker =:cross)
+    p1 = plot!(θ*180/π,   ε̇ii_rot, label="ε̇ii_rot",  linewidth=0, marker =:circle)
 end
-
-
 
 main_simple_ani_vis()
