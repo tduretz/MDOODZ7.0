cf4d059258301825b7e059c7c4f32a7770e4edd1
tduretz <duretz@uni-frankfurt.de>
2023-03-30 17:39:27 +0200

    add grain size visualisation

--- misc/Python_visualisation/Read_Doodz_GrainSize.py
+++ misc/Python_visualisation/Read_Doodz_GrainSize.py
@@ -8,11 +8,11 @@ home = expanduser("~")
 path ="/REPO/MDOODZ7.0/MDLIB/"
 
 # File numbers
-file_start = 10
+file_start = 80
 file_step  = 10
-file_end   = 10
+file_end   = 80
 
-viz_X = True
+viz_d = True
 
 for step in range(file_start, file_end+1, file_step):
     filename    = home + path + 'Output%05d' % step + '.gzip.h5'
@@ -23,14 +23,14 @@ for step in range(file_start, file_end+1, file_step):
     zv_ph  = file['/VizGrid/zviz_hr']
     data   = file['/Model/Params']
 
-    X     = file['/Centers/X']
+    d     = file['/Centers/d']
     xc    = file['/Model/xc_coord']
     zc    = file['/Model/zc_coord']
     Nx    = data[3].astype(int)
     Nz    = data[4].astype(int)
     Ncx   = Nx-1
     Ncz   = Nz-1
-    X     = np.reshape( X, (Ncz, Ncx)) 
+    d     = np.reshape( d, (Ncz, Ncx)) 
 
     # Achtung the phase map has a twice finer resolution
     xc_ph  = 0.5*(xv_ph[0:-1] + xv_ph[1:])
@@ -39,21 +39,25 @@ for step in range(file_start, file_end+1, file_step):
     Ncz_ph = zc_ph.shape[0]
     phases = np.reshape(phases,(Ncz_ph,Ncx_ph))
 
-    if viz_X:
+    if viz_d:
         mpl.clf()
-        mpl.pcolor(xc, zc, X)
+        ax = mpl.subplot(1,1,1)
+        mpl.pcolor(xc, zc, np.log10(d/1e-6), vmin=1, vmax=3)
         mpl.set_cmap('jet')
         mpl.colorbar()
         mpl.tight_layout()
+        ax.set_xlim(-0.4, 0.4)
+        ax.set_ylim(-0.175, 0.175)
+        mpl.title('d') 
         mpl.show()
-        mpl.title('X')     
+    
     else:
         mpl.clf()
         mpl.pcolor(xc_ph, zc_ph, phases)
         mpl.set_cmap('jet')
         mpl.colorbar()
         mpl.tight_layout()
-        mpl.show()
         mpl.title('Phases')
+        mpl.show()
 
     file.close()                             
