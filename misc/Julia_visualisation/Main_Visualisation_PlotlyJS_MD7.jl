e6cb90bef8faf81c470212b6c3fcd48396959d78
tduretz <imac@geo145.geophysik.uni-frankfurt.de>
2023-04-12 16:15:51 +0200

    tiny update

--- misc/Julia_visualisation/Main_Visualisation_PlotlyJS_MD7.jl
+++ misc/Julia_visualisation/Main_Visualisation_PlotlyJS_MD7.jl
@@ -1,9 +1,13 @@
-import Pkg
-Pkg.activate(normpath(joinpath(@__DIR__, ".")))
-using HDF5, Plots, Printf
-plotlyjs()
+using HDF5, PlotlyJS, Printf
 
-function main()
+function ExtractData( file_path, data_path)
+    data = h5open(file_path, "r") do file
+        read(file, data_path)
+    end
+    return data
+end
+
+function main_PlotlyJS()
 
     path ="/Users/imac/REPO_GIT/MDOODZ7.0/MDLIB/"
     # path ="/Users/tduretz/REPO/MDOODZ7.0/MDLIB/MD67_assembly_LR/"
@@ -24,12 +28,12 @@ function main()
     field = :density
     field = :viscosity
     # field = :pl_srate
-    # field = :pressure
-    field = :velocity_x
-    field = :velocity_z
+    field = :pressure
+    # field = :velocity_x
+    # field = :velocity_z
     # field = :grain_size
 
-    phase_contours = false
+    phase_contours = true
 
     for istep=file_start:file_step:file_end
     
@@ -45,6 +49,8 @@ function main()
         zc_ph  = 0.5.*(zv_ph[1:end-1] .+ zv_ph[2:end])
         ncx_ph = length(xc_ph)
         ncz_ph = length(zc_ph)
+        xmin, xmax = xc[1], xc[end]
+        zmin, zmax = zc[1], zc[end]
 
         @show model
         t   = model[1]
@@ -64,31 +70,32 @@ function main()
         Vxc = 0.5 .* (Vx[1:end-1,2:end-1] .+ Vx[2:end-0,2:end-1])
         Vzc = 0.5 .* (Vz[2:end-1,1:end-1] .+ Vz[2:end-1,2:end-0])
 
-        p = plot()
+        # p = plot()
 
-        if field==:phases      p = heatmap!(xc_ph./Lc, zc_ph./Lc, ph', c=:turbo)   end
-        if field==:density     p = heatmap!(xc./Lc, zc./Lc, (ρc'), c=:turbo) end
-        if field==:viscosity   p = heatmap!(xc./Lc, zc./Lc, log10.(ηc'), c=:turbo) end
-        if field==:pl_srate    p = heatmap!(xc./Lc, zc./Lc, log10.(ε̇pl'), c=:turbo) end
-        if field==:pressure    p = heatmap!(xc./Lc, zc./Lc, (P'), c=:turbo) end
-        if field==:grain_size  p = heatmap!(xc./Lc, zc./Lc, log10.(d'), c=:turbo) end
-        if field==:velocity_x  p = heatmap!(xc./Lc, zc./Lc, Vxc', c=:turbo, clims=(-0.05, 0.05)) end
-        if field==:velocity_z  p = heatmap!(xc./Lc, zc./Lc, Vzc', c=:turbo, clims=(-0.2, 0.1)) end
+        # if field==:phases      p = heatmap!(xc_ph./Lc, zc_ph./Lc, ph', c=:turbo)   end
+        # if field==:density     p = heatmap!(xc./Lc, zc./Lc, (ρc'), c=:turbo) end
+        # if field==:viscosity   p = heatmap!(xc./Lc, zc./Lc, log10.(ηc'), c=:turbo) end
+        # if field==:pl_srate    p = heatmap!(xc./Lc, zc./Lc, log10.(ε̇pl'), c=:turbo) end
+        # if field==:pressure    p = heatmap!(xc./Lc, zc./Lc, (P'), c=:turbo) end
+        # if field==:grain_size  p = heatmap!(xc./Lc, zc./Lc, log10.(d'), c=:turbo) end
+        # if field==:velocity_x  p = heatmap!(xc./Lc, zc./Lc, Vxc', c=:turbo) end
+        # if field==:velocity_z  p = heatmap!(xc./Lc, zc./Lc, Vzc', c=:turbo) end
 
-        if phase_contours  p = contour!(xc_ph./1e3, zc_ph./1e3, ph', c=:black) end
-        p = plot!(aspect_ratio=1)
+        # if phase_contours  p = contour!(xc_ph./1e3, zc_ph./1e3, ph', c=:black) end
+        # p = plot!(aspect_ratio=1)
+
+        p = Plot([PlotlyJS.heatmap(x=xc_ph./Lc, y=zc_ph./Lc, z=P', colorscale=colors.turbo, colorbar_thickness=24), 
+        #PlotlyJS.contour(x=xc_ph./1e3, y=zc_ph./1e3, z=ph',
+                #line_width=2, line_color="black")
+                ],
+        PlotlyJS.Layout(xaxis_range=[-3, 3], yaxis_range=[zmin/Lc, zmax/Lc], height=400, yaxis_scaleanchor="x", yaxis_scaleratio=1))
+        
         display(p)
         sleep(0.1)
         @show P[1]
+
     end
 
 end
 
-function ExtractData( file_path, data_path)
-    data = h5open(file_path, "r") do file
-        read(file, data_path)
-    end
-    return data
-end
-
-main()
+main_PlotlyJS()
