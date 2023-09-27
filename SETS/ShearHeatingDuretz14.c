1b57b4fc480bae4febfa2fcc9648dc7b2752a71c
tduretz <duretz@uni-frankfurt.de>
2023-09-27 14:27:28 +0200

    revise anisotropic shear heating

--- SETS/ShearHeatingDuretz14.c
+++ SETS/ShearHeatingDuretz14.c
@@ -1,4 +1,8 @@
+#include "math.h"
 #include "mdoodz.h"
+#include "stdbool.h"
+#include "stdlib.h"
+#include "stdio.h"
 
 int SetPhase(MdoodzInput *input, Coordinates coordinates) {
   const double radius = input->model.user1 / input->scaling.L;
@@ -31,7 +35,16 @@ SetBC SetBCT(MdoodzInput *instance, POSITION position, double particleTemperatur
   return bc;
 }
 
-int main() {
+int main(int nargs, char *args[]) {
+  // Input file name
+  char *input_file;
+  if ( nargs < 2 ) {
+    asprintf(&input_file, "AnisoViscTest_HomoRandomAngle.txt"); // Default
+  }
+  else {
+    asprintf(&input_file, "%s", args[1]);     // Custom
+  }
+  printf("Running MDoodz7.0 using %s\n", input_file);
   MdoodzSetup input = {
           .SetParticles = &(SetParticles_ff){
                   .SetPhase       = SetPhase,
@@ -44,5 +57,6 @@ int main() {
                   .SetBCT    = SetBCT,
           },
   };
-  RunMDOODZ("ShearHeatingDuretz14.txt", &input);
+  RunMDOODZ(input_file, &input);
+  free(input_file);
 }
