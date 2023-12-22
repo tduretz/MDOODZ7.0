f0b30e4854b973a7857c0aa170f015cc48412e4f
kulakovri <49407208+kulakovri@users.noreply.github.com>
2023-12-22 15:12:20 +0100

    Add transmutation parameters to input file (#120)

--- MDLIB/ParticleRoutines.c
+++ MDLIB/ParticleRoutines.c
@@ -3204,3 +3204,25 @@ void CountPartCell ( markers* particles, grid *mesh, params model, surface topo,
 /*--------------------------------------------------------------------------------------------------------------------*/
 /*------------------------------------------------------ M-Doodz -----------------------------------------------------*/
 /*--------------------------------------------------------------------------------------------------------------------*/
+
+void TransmutateMarkers(markers *particles, mat_prop *materials, double scaling_T) {
+#pragma omp parallel for shared ( particles )
+    int transmutated_particles = 0;
+    for (int k=0; k<particles->Nb_part-1; k++) {
+        const int phase = particles->phase[k];
+        const double temperature = (particles->T[k] * scaling_T) - zeroC;
+        const double transmutation = materials->transmutation[phase];
+
+        if ((transmutation == -1 && materials->transmutation_temperature[phase] >= temperature)
+            || (transmutation == 1 && materials->transmutation_temperature[phase] <= temperature)) {
+            particles->phase[k] = materials->transmutation_phase[phase];
+            particles->dual[k] = materials->transmutation_phase[phase];
+            transmutated_particles++;
+        }
+    }
+    printf("Transmutated particles = %03d\n", transmutated_particles);
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+/*------------------------------------------------------ M-Doodz -----------------------------------------------------*/
+/*--------------------------------------------------------------------------------------------------------------------*/
