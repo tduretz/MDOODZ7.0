f8e069240c026e09141a12ef707faaf64cff5e25
kulakovri <kulakovri@gmail.com>
2024-02-13 14:40:14 -0500

    covert nx and nz ro degrees

--- JuliaVisualisation/crust_particles.py
+++ JuliaVisualisation/crust_particles.py
@@ -2,8 +2,7 @@ import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 
-My     = 1e6*3600*24*365.25
-
+My = 1e6 * 3600 * 24 * 365.25
 
 def read_particle_data(filename, indices=None):
     with h5py.File(filename, 'r') as file:
@@ -23,27 +22,28 @@ def read_particle_data(filename, indices=None):
 
 
 def calculate_angles(nx, nz):
-    angles_rad = np.arctan2(nz, nx)
+    n = np.sqrt(nz ** 2 + nz ** 2)
+
+    # Calculate the angles in radians
+    angles_rad = np.arctan2(nz / n, nx / n)
+
+    # Convert to degrees
     angles_deg = np.degrees(angles_rad)
-    angles_deg = np.mod(angles_deg, 360)
-    angles_deg[angles_deg > 180] -= 360
-    angles_deg = np.abs(angles_deg)
+    angles_deg[angles_deg < 90] += 180
     return angles_deg
 
 
 def main():
-    # Adjust plot layout
     plt.subplots_adjust(bottom=0.80, left=0.50, top=0.99)
-
-    # Define file pattern and range
     base_path = "C:\\Users\\rkulakov\\CLionProjects\\MDOODZ7.0\\cmake-exec\\NeckingReview\\particles\\"
     file_pattern = "Particles{:05d}.gzip.h5"
-    steps = range(0, 12, 2)  # From 0 to 10 inclusive, with a step of 2
+    steps = range(0, 12, 2)
 
     initial_indices = None
     positions = []
     mean_positions = []
     angles = []
+    all_angles = []
 
     for step in steps:
         filename = base_path + file_pattern.format(step)
@@ -52,6 +52,7 @@ def main():
 
         if initial_indices is None:
             initial_indices = (x > 5000) & (x < 6000) & (z < -3000) & (z > -4000)
+            initial_indices = (x > 15000) & (x < 16000) & (z < -13000) & (z > -14000)
             x, z, nx, nz = x[initial_indices], z[initial_indices], nx[initial_indices], nz[initial_indices]
 
         angles_deg = calculate_angles(nx, nz)
@@ -59,9 +60,9 @@ def main():
         mean_positions.append((np.mean(x), np.mean(z)))
         positions.append((x, z))
         angles.append(angles_deg)
+        all_angles.append(angles_deg)
 
-    true_count = np.sum(initial_indices)
-
+    # Assuming all_angles is now a list where each item is an array of angles for each step
     # Plotting
     fig, axs = plt.subplots(2, 1, figsize=(10, 8))
 
@@ -82,6 +83,12 @@ def main():
     axs[1].set_title('Particle Angle Changes')
     axs[1].set_xlabel('Step')
     axs[1].set_ylabel('Angle (degrees)')
+    axs[1].set_ylim([0, 360])
+
+    # For each particle, plot its angle change across steps
+    for particle_index in range(len(all_angles[0])):
+        particle_angles = [step_angles[particle_index] for step_angles in all_angles]
+        axs[1].plot(steps, particle_angles, '-o', markersize=4)
 
     plt.tight_layout()
     plt.show()
