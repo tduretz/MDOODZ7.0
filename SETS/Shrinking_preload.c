a7009e7f66470f436c0b3603aa9b22bbdea803f2
mthielma <thielmann.marcel@gmail.com>
2024-02-26 18:42:16 +0100

    added ellipsoidal inclusion

--- SETS/Shrinking_preload.c
+++ SETS/Shrinking_preload.c
@@ -1,6 +1,7 @@
 #include "mdoodz.h"
 #include "math.h"
 
+/*
 int SetPhase(MdoodzInput *input, Coordinates coordinates) {
   const double radius = 0.01 / input->scaling.L;
   if (coordinates.x * coordinates.x + coordinates.z * coordinates.z < radius * radius) {
@@ -8,6 +9,39 @@ int SetPhase(MdoodzInput *input, Coordinates coordinates) {
   } else {
     return 0;
   }
+}
+*/
+
+// for the ellipse, we have several arguments: effective radius, aspect ratio, orientation, X0,Z0
+int SetPhase(MdoodzInput *input, Coordinates coordinates) {
+    double radius = 0.01 / input->scaling.L;
+    double AR = 2;
+    double a,b, Xeff,Zeff;
+    double cos_theta = cos(45*M_PI/180.0); 
+    double sin_theta=sin(45*M_PI/180.0);
+    double X0=0;
+    double Z0=0;
+
+    if (AR>1){
+      // compute half axis lengths so that we have the same "volume" as a circle with radius
+      b   = radius / sqrt(AR);
+      a   = radius * sqrt(AR);
+    
+      Xeff = (coordinates.x-X0)*cos_theta + (coordinates.z-Z0)*sin_theta;
+      Zeff = (coordinates.x-X0)*sin_theta - (coordinates.z-Z0)*cos_theta;
+    } 
+    else {
+      b = radius;
+      a = radius;
+      Xeff = coordinates.x-X0;
+      Zeff = coordinates.z-Z0;
+    }
+
+    if ( Xeff*Xeff/(a*a) +Zeff*Zeff/(b*b) <1) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
 
 int SetDualPhase(MdoodzInput *input, Coordinates coordinate, int phase) {
