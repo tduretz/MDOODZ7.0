017802d1bd26d517ed49bae4f64344d0bd6e2300
tduretz <duretz@uni-frankfurt.de>
2024-10-18 20:23:34 +0200

    make coordinates accessible from SetBC routines

--- SETS/RiftingMelting.c
+++ SETS/RiftingMelting.c
@@ -12,20 +12,23 @@ int SetDualPhase(MdoodzInput *input, Coordinates coordinate, int phase) {
     double Ax, Az;
 
     // Set checkerboard for phase 0
-    Az = sin( 48.0*2.0*M_PI*coordinate.z / Lz  );
-    if ( Az>0.0 && dual_phase==0 ) {
+    Ax = cos(         36.0*2.0*M_PI*coordinate.x / Lx  );
+    Az = sin( (Lz/Lx)*36.0*2.0*M_PI*coordinate.z / Lz  );
+    if ( ( (Az<0.0 && Ax<0.0) || (Az>0.0 && Ax>0.0) ) && dual_phase==0 ) {
         dual_phase += input->model.Nb_phases;
     }
 
     // Set checkerboard for phase 1
-    Az = sin( 48.0*2.0*M_PI*coordinate.z / Lz  );
-    if ( Az>0.0 && dual_phase==1 ) {
+    Ax = cos(         36.0*2.0*M_PI*coordinate.x / Lx  );
+    Az = sin( (Lz/Lx)*36.0*2.0*M_PI*coordinate.z / Lz  );
+    if ( ( (Az<0.0 && Ax<0.0) || (Az>0.0 && Ax>0.0) ) && dual_phase==1 ) {
         dual_phase += input->model.Nb_phases;
     }
 
     // Set checkerboard for phase 2
-    Az = sin( 48.0*2.0*M_PI*coordinate.z / Lz  );
-    if ( Az>0.0 && dual_phase==2 ) {
+    Ax = cos(         36.0*2.0*M_PI*coordinate.x / Lx  );
+    Az = sin( (Lz/Lx)*36.0*2.0*M_PI*coordinate.z / Lz  );
+    if ( ( (Az<0.0 && Ax<0.0) || (Az>0.0 && Ax>0.0) ) && dual_phase==2 ) {
         dual_phase += input->model.Nb_phases;
     }
 
@@ -33,33 +36,36 @@ int SetDualPhase(MdoodzInput *input, Coordinates coordinate, int phase) {
 }
 
 
-double SetSurfaceZCoord(MdoodzInput *instance, double x_coord) {
-  const double TopoLevel   = -0.0e3 / instance->scaling.L;
-  const double basin_width = 30.0e3 / instance->scaling.L;
-  const double h_pert      = instance->model.user3 / instance->scaling.L;
-  const double x           = x_coord - instance->model.user4 / instance->scaling.L;
+double SetSurfaceZCoord(MdoodzInput *input, double x_coord) {
+  const double TopoLevel   = -0.0e3 / input->scaling.L;
+  const double basin_width = 30.0e3 / input->scaling.L;
+  const double h_pert      = input->model.user3 / input->scaling.L;
+  const double x           = x_coord - input->model.user4 / input->scaling.L;
   return TopoLevel + h_pert * exp( - (x*x) / (2.0*basin_width*basin_width)   );
 }
 
-double SetNoise(MdoodzInput *instance, Coordinates coordinates, int phase) {
-  const double x        = coordinates.x - instance->model.user4 / instance->scaling.L;
+double SetNoise(MdoodzInput *input, Coordinates coordinates, int phase) {
+  const double x        = coordinates.x - input->model.user4 / input->scaling.L;
   const double z        = coordinates.z;
-  const double basin_width = 30.0e3 / instance->scaling.L;
+  const double basin_width = 30.0e3 / input->scaling.L;
   const double noise = ((double) rand() / (double) RAND_MAX) - 0.5;
   const double filter_x = exp( - (x*x)/ (2.0*basin_width*basin_width) );
   const double filter_z = exp( - (z*z)/ (2.0*basin_width*basin_width*4.0) );
   return  noise * filter_x * filter_z;
 }
 
-int SetPhase(MdoodzInput *instance, Coordinates coordinates) {
-  const double basin_width           = 30.0e3 / instance->scaling.L;
-  const double h_pert                = instance->model.user3 / instance->scaling.L;
-  const double lithosphereThickness  = instance->model.user1 / instance->scaling.L;
-  const double crustThickness        = instance->model.user2 / instance->scaling.L;
-  const double perturbationAmplitude = instance->model.user3 / instance->scaling.L;
-  const double x                     = coordinates.x - instance->model.user4 / instance->scaling.L;
+int SetPhase(MdoodzInput *input, Coordinates coordinates) {
+  const double basin_width           = 30.0e3 / input->scaling.L;
+  const double h_pert                = input->model.user3 / input->scaling.L;
+  const double lithosphereThickness  = input->model.user1 / input->scaling.L;
+  const double crustThickness        = input->model.user2 / input->scaling.L;
+  const double perturbationAmplitude = input->model.user3 / input->scaling.L;
+  const double x                     = coordinates.x;// - input->model.user4 / input->scaling.L;
   const double mohoLevel             = -crustThickness + 0*h_pert * exp( - (x*x) / (2.0*basin_width*basin_width)   );
-  const bool   isBelowLithosphere    = coordinates.z < -lithosphereThickness;
+  const double w                     = input->model.user5 / input->scaling.L;
+  const double h_LAB                 = 10e3 / input->scaling.L;
+  const double LABLevel              = -lithosphereThickness + h_LAB * exp( -(x*x) / (2.0*w*w)   );
+  const bool   isBelowLithosphere    = coordinates.z < LABLevel;
   const bool   isAboveMoho           = coordinates.z > mohoLevel;
   const bool   isLowerCrust          = coordinates.z < (mohoLevel + 0.5*crustThickness);
 
@@ -77,14 +83,14 @@ int SetPhase(MdoodzInput *instance, Coordinates coordinates) {
   }
 }
 
-double SetTemperature(MdoodzInput *instance, Coordinates coordinates) {
-  const double lithosphereThickness = instance->model.user1 / instance->scaling.L;
-  const double surfaceTemperature   = 273.15 / instance->scaling.T;
-  const double mantleTemperature    = (1330.0 + 273.15) / instance->scaling.T;
-  const double Tamp               = 50.0 / instance->scaling.T;
-  const double x                  = coordinates.x - instance->model.user4 / instance->scaling.L;
+double SetTemperature(MdoodzInput *input, Coordinates coordinates) {
+  const double lithosphereThickness = input->model.user1 / input->scaling.L;
+  const double surfaceTemperature   = 273.15 / input->scaling.T;
+  const double mantleTemperature    = (1330.0 + 273.15) / input->scaling.T;
+  const double Tamp               = 50.0 / input->scaling.T;
+  const double x                  = coordinates.x - input->model.user4 / input->scaling.L;
   const double z                  = coordinates.z;
-  const double basin_width        = 30.0e3 / instance->scaling.L;
+  const double basin_width        = 30.0e3 / input->scaling.L;
   const double filter_x           = Tamp * exp( - (x*x)/ (2.0*basin_width*basin_width) );
   
   const double particleTemperature  = ((mantleTemperature - surfaceTemperature) / lithosphereThickness) * (-coordinates.z) + surfaceTemperature;
@@ -95,12 +101,12 @@ double SetTemperature(MdoodzInput *instance, Coordinates coordinates) {
   }
 }
 
-double SetGrainSize(MdoodzInput *instance, Coordinates coordinates, int phase) {
+double SetGrainSize(MdoodzInput *input, Coordinates coordinates, int phase) {
   const int asthenospherePhase = 3;
-  return instance->materials.gs_ref[asthenospherePhase];
+  return input->materials.gs_ref[asthenospherePhase];
 }
 
-char SetBCPType(MdoodzInput *instance, POSITION position) {
+char SetBCPType(MdoodzInput *input, POSITION position) {
   if (position == NE || position == NW) {
     return 0;
   } else {
@@ -108,16 +114,20 @@ char SetBCPType(MdoodzInput *instance, POSITION position) {
   }
 }
 
-SetBC SetBCT(MdoodzInput *instance, POSITION position, double particleTemperature) {
+SetBC SetBCT(MdoodzInput *input, POSITION position, Coordinates coordinates, double particleTemperature) {
   SetBC     bc;
-  double surface_temperature =          zeroC  / instance->scaling.T;
-  double mantle_temperature  = (1330. + zeroC) / instance->scaling.T;
-  double mantle_heatflux     =  -55e-3 / (instance->scaling.W/pow(instance->scaling.L,2));
+  const double surface_temperature =          zeroC  / input->scaling.T;
+  const double mantle_temperature  = (1330. + zeroC) / input->scaling.T;
+  const double qz0 =  -48e-3 / (input->scaling.W/pow(input->scaling.L,2));
+  const double qz1 =   input->model.user6  / (input->scaling.W/pow(input->scaling.L,2));
+  const double w   =   input->model.user5 /input->scaling.L; 
+  const double qz  = qz0 + qz1 * exp( -(coordinates.x*coordinates.x)/(2*w*w) );
+  // printf("%2.2e %2.2e\n", coordinates.x, qz*(input->scaling.W/pow(input->scaling.L,2)));
   if (position == S) {
     // bc.type  = constant_temperature;
     // bc.value = particleTemperature;
     bc.type  = constant_heatflux;
-    bc.value = mantle_heatflux;
+    bc.value = qz;
   }
   if (position == free_surface || position == N) {
     bc.type  = constant_temperature;
