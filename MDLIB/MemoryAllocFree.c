6c300059eb115deb44f55340a2379177672ae866
Lilou Zeller <lilou.zeller@umontpellier.fr>
2026-07-10 11:09:30 +0200

    isotropic damage but nit used in rheology computation

--- MDLIB/MemoryAllocFree.c
+++ MDLIB/MemoryAllocFree.c
@@ -264,6 +264,8 @@ markers PartAlloc(ParticlesInput particlesInput, params *model) {
   // mirroring strain_pwl (PartInit sets the isotropic δ = 1 start).
   particles.aniso_delta         = DoodzCalloc(particles.Nb_part_max, sizeof(DoodzFP));
   particles.aniso_delta_fs_prev = DoodzCalloc(particles.Nb_part_max, sizeof(DoodzFP));
+  // CLZ particles.eta  = DoodzCalloc(particles.Nb_part_max, sizeof(DoodzFP));
+
 
   particles.intag      = DoodzCalloc(particles.Nb_part_max, sizeof(int));
 
@@ -341,6 +343,7 @@ void PartFree( markers *particles, params* model ) {
 
     DoodzFree(particles->aniso_delta);
     DoodzFree(particles->aniso_delta_fs_prev);
+    // CLZ DoodzFree(particles->eta);
 
     DoodzFree(particles->intag);
 
